df81fc2a25b17dd5cc84e07b7e568fdee62dd8ab
smrmkt <smrmkt@gmail.com>
2015-05-09 20:35:57 +0900

    add randomforest prediction sctipt and gitignore setting

---
+++ r/src/randomforest.R
@@ -0,0 +1,26 @@
+require('randomForest')
+require('data.table')
+require('pforeach')
+
+# load data
+feature_train = fread('./data/feature_train.csv')
+feature_test = fread('./data/feature_test.csv')
+truth_train = fread('./data/truth_train.csv')
+setnames(truth_train, colnames(truth_train), c('enrollment_id', 'dropout'))
+data_train = merge(truth_train, feature_train, by='enrollment_id')
+
+# randomforest model
+fit.rf = pforeach(ntree=rep(250, .cores), .c=combine)({
+  randomForest(dropout~., data=data_train, ntree=ntree)
+})
+
+# predict test data
+predict.rf = predict(fit.rf, feature_test)
+predict.rf.b = as.numeric(predict.rf > 0.5)
+predict.rf.out = as.data.frame(
+  cbind(feature_test$enrollment_id, predict.rf.b))
+setnames(predict.rf.out, colnames(predict.rf.out), c('enrollment_id', 'dropout'))
+write.table(predict.rf.out,
+            "./data/predict.rf.out.csv", 
+            sep=',', col.names=F, row.names=F, quote=F)
+
