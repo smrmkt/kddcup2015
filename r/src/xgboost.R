800afcd342b1849443b364d546161b0705926e36
smrmkt <smrmkt@gmail.com>
2015-05-24 16:36:21 +0900

    add xgboost prediction

---
+++ r/src/xgboost.R
@@ -0,0 +1,51 @@
+require('data.table')
+require('xgboost')
+
+# load data
+## train data
+train.feature.enrollment = fread('./data/feature/enrollment_feature_train.csv')
+train.feature.user = fread('./data/feature/user_feature_train.csv')
+train.feature = merge(train.feature.enrollment,
+                      train.feature.user, by='enrollment_id')
+train.truth = fread('./data/feature/truth_train.csv')
+train.truth = train.truth[1:nrow(train.feature),]
+setnames(train.truth, colnames(train.truth), c('enrollment_id', 'dropout'))
+train.dataset = merge(train.feature, train.truth, by='enrollment_id')
+train.dataset$enrollment_id = NULL
+## test data
+test.feature.enrollment = fread('./data/feature/enrollment_feature_test.csv')
+test.feature.user = fread('./data/feature/user_feature_test.csv')
+test.feature = merge(test.feature.enrollment,
+                     test.feature.user, by='enrollment_id')
+
+# xgboost
+param = list('objective'= 'binary:logistic',
+             'scale_pos_weight'=ratio,
+             'bst:eta'=0.1,
+             'bst:max_depth'=4,
+             'eval_metric'='auc',
+             'silent' = 1,
+             'nthread' = 16)
+train.cv = xgb.cv(param=param,
+                  as.matrix(train.feature.stacking),
+                  label=train.truth$dropout,
+                  nfold=round(1+log2(nrow(train.feature))),
+                  nrounds=100)
+nround = which.max(train.cv$test.auc.mean)
+xgmat = xgb.DMatrix(as.matrix(train.feature),
+                    label=train.truth$dropout,
+                    weight=train.weights,
+                    missing=-999.0)
+watchlist = list("train"=xgmat)
+train.fit = xgb.train(param, xgmat, nround) #, watchlist)
+
+# predict test data
+test.predict = predict(train.fit, as.matrix(test.feature))
+test.predict.out = as.data.frame(cbind(test.feature$enrollment_id, test.predict))
+setnames(test.predict.out,
+         colnames(test.predict.out),
+         c('enrollment_id', 'dropout'))
+write.table(test.predict.out,
+            './data/predict/predict.xgboost.csv', 
+            sep=',', col.names=F, row.names=F, quote=F)
+
